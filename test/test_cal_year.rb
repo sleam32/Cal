d2733b8a8fb6098324a949b33a4da9a7a2366f3b
Shearry Malone <sleam32@gmail.com>
2013-05-04 12:39:51 -0500

    corrected some indention

--- test/test_cal_year.rb
+++ test/test_cal_year.rb
@@ -8,16 +8,16 @@ class YearUnitTests < Test::Unit::TestCase
 def test_year_header
     cal = Year.new(2012)
     assert_equal("                              2012\n", cal.test_header)
-  end
+  	end
 
-  def test_month_header
+def test_month_header
   	cal = Year.new(2012)
   	assert_equal(%w[January February March April May June July August September October November December], cal.month_header)
 	end
-	
-  def test_days_header
+
+def test_days_header
   	cal = Year.new(2012)
   	assert_equal("Su Mo Tu We Th Fr Sa", cal.days_header)
-  end
+  	end
 
 end
--- test/year.rb
+++ test/year.rb
@@ -1,9 +1,9 @@
 class Year
 
 
-	def initialize (year) 
+def initialize (year) 
 	@year = year
-end
+	end
 
 def test_header
 	string = "#{@year}".center(64).rstrip + "\n"
@@ -14,9 +14,9 @@ def month_header
 	month_array = []
 	month_header = "January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "Novemeber", "December"
 	month_array << month_header
-end
+	end
 
-	def days_header
+def days_header
 		"Su Mo Tu We Th Fr Sa"
 	end
 end
